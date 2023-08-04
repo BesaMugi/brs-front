2ce5e427ae17b827e2520e25d7221dcb1a42dfd5
ali <loading.03@mail.ru>
2023-08-04 14:27:18 +0300

    закончил с выводом пользователей

--- src/components/AllUsers/index.tsx
+++ src/components/AllUsers/index.tsx
@@ -1,99 +1,46 @@
-import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
-import { RootState } from "../../store/store";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { userAll } from "../../reducer/userSlice";
+import { RootState, AppDispatch } from "store/store";
 
-// Определите тип данных, которые возвращает ваше асинхронное действие userInfo
-type UserData = {
+interface User {
   id: number;
-  name: string;
-  // ... другие свойства пользователя ...
-};
-
-// Создайте асинхронное действие userInfo
-export const userInfo = createAsyncThunk<UserData[], void, { state: RootState }>(
-  "user/userInfo",
-  async (_, thunkAPI) => {
-    try {
-      const res = await fetch("http://localhost:3000/profile", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${thunkAPI.getState().application.token}`,
-        },
-      });
-      const json = await res.json();
-      return json;
-    } catch (error) {
-      throw new Error(error.message || "Ошибка получения данных пользователя");
-    }
-  }
-);
-
-// Определите тип данных, которые возвращает ваше асинхронное действие userAll
-type AllUsersData = {
-  id: number;
-  name: string;
-  // ... другие свойства пользователей ...
-};
-
-// Создайте асинхронное действие userAll
-export const userAll = createAsyncThunk<AllUsersData[], void>(
-  "user/userAll",
-  async () => {
-    try {
-      const res = await fetch("http://localhost:3000/users");
-      const users = await res.json();
-      return users;
-    } catch (error) {
-      throw new Error(error.message || "Ошибка получения списка пользователей");
-    }
-  }
-);
-
-// Определите интерфейс состояния среза
-interface UserState {
-  users: UserData[];
-  loading: boolean;
-  error: string | null;
+  login: string;
+  lastname: string;
+  surname: string;
+  group: string;
+  role: string;
+  password: string;
 }
 
-// Начальное состояние среза
-const initialState: UserState = {
-  users: [],
-  loading: false,
-  error: null,
+const AllUsers: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const users = useSelector<RootState, User[]>((state) => state.users.users);
+  const loading = useSelector<RootState, boolean>((state) => state.users.loading);
+  const error = useSelector<RootState, null | string>((state) => state.users.error);
+
+  useEffect(() => {
+    dispatch(userAll());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Загрузка...</div>;
+  }
+
+  if (error) {
+    return <div>Ошибка: {error}</div>;
+  }
+
+  return (
+    <div>
+      <h2>Пользователи</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.login}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(userInfo.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(userInfo.fulfilled, (state, action) => {
-        state.loading = false;
-        state.error = null;
-        state.users = action.payload;
-      })
-      .addCase(userInfo.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message || "Ошибка получения данных пользователя";
-      })
-      .addCase(userAll.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(userAll.fulfilled, (state, action) => {
-        state.loading = false;
-        state.error = null;
-        state.users = action.payload;
-      })
-      .addCase(userAll.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message || "Ошибка получения списка пользователей";
-      });
-  },
-});
-
-export default userSlice.reducer;
+export default AllUsers;
--- src/reducer/userSlice.tsx
+++ src/reducer/userSlice.tsx
@@ -1,41 +1,66 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from "../store/store";
 
-const userSlice = createSlice({
-  name: "user",
-  initialState: {
-    users: [], 
+type UserData = {
+  id: number;
+  login: string;
+  lastname: string;
+  surname: string;
+  group: string;
+};
+
+type AllUsersData = {
+  id: number;
+  login: string;
+  lastname: string;
+  surname: string;
+  group: string;
+};
+
+export const userAll = createAsyncThunk<AllUsersData[], void>(
+  "user/userAll",
+  async () => {
+    try {
+      const res = await fetch("http://localhost:3000/users");
+      const users = await res.json();
+      return users;
+    } catch (error) {
+      throw new Error(error.message || "Ошибка получения списка пользователей");
+    }
+  }
+);
+
+interface UserState {
+    users: UserData[];
+    loading: boolean;
+    error: string | null;
+  }
+  
+  const initialState: UserState = {
+    users: [],
     loading: false,
     error: null,
-  },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(userInfo.pending, (state) => {
-      state.loading = true;
-    });
-    builder.addCase(userInfo.fulfilled, (state, action) => {
-      state.loading = false;
-      state.error = null;
-      state.users = action.payload;
-    });
-    builder.addCase(userInfo.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.error.message || "Ошибка получения данных пользователя";
-    });
+  };
+  
+  const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+      builder
+        .addCase(userAll.pending, (state) => {
+          state.loading = true;
+        })
+        .addCase(userAll.fulfilled, (state, action) => {
+          state.loading = false;
+          state.error = null;
+          state.users = action.payload;
+        })
+        .addCase(userAll.rejected, (state, action) => {
+          state.loading = false;
+          state.error = action.error.message || "Ошибка получения списка пользователей";
+        });
+    },
+  });
 
-    builder.addCase(userAll.pending, (state) => {
-      state.loading = true;
-    });
-    builder.addCase(userAll.fulfilled, (state, action) => {
-      state.loading = false;
-      state.error = null;
-      state.users = action.payload;
-    });
-    builder.addCase(userAll.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.error.message || "Ошибка получения списка пользователей";
-    });
-  },
-});
-
-export default userSlice.reducer;
+  export default userSlice.reducer
