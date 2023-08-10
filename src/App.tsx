58ef272fd3b93d46e19e797fd36c8ccf81ca807a
ali <loading.03@mail.ru>
2023-08-10 14:03:02 +0300

    доработал журнал

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import Lessons from "./pages/Lessons/index";
 import Group from "./pages/Group/index";
-import Brs from "./pages/JournalActive/index";
+import Brs from "./pages/JournalActive";
 import { useSelector } from "react-redux";
 import { RootState } from "store/store";
 import GroupList from "./pages/Group/GroupList";
 import HomeMain from "./components/Main/HomeMain";
 import Journal from "./pages/JournalPresent";
+import SubjectJournal from "./pages/JournalPresent/SubjectJournal";
 
 function App() {
   const token = useSelector((state: RootState) => state.application.token);
@@ -33,6 +34,10 @@ function App() {
       <Route path="/groups" element={<Group />} />
       <Route path="/group/:id" element={<GroupList />} />
       <Route path="/journal" element={<Journal />} />
+      <Route
+        path="/groups/:groupId/subjects/:subjectId"
+        element={<SubjectJournal />}
+      />
     </Routes>
   );
 }
